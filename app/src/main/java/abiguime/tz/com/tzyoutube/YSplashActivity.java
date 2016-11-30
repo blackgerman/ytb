5a7d561d84175387457e18d61e41b74612d961cd
blackgerman <ulrichabiguime@gmail.com>
2016-11-30 21:29:44 +0800

    first commit

---
+++ app/src/main/java/abiguime/tz/com/tzyoutube/YSplashActivity.java
@@ -0,0 +1,42 @@
+package abiguime.tz.com.tzyoutube;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class YSplashActivity extends AppCompatActivity
+implements Runnable{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ysplash);
+
+    // Example of a call to a native method
+//    TextView tv = (TextView) findViewById(R.id.sample_text);
+//    tv.setText(stringFromJNI());
+
+        Handler mhandler = new Handler();
+        mhandler.postDelayed(this, 3000);
+    }
+
+    @Override
+    public void run() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        this.finish();
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+//    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
+}
